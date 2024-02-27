c42598b59d54361e11514df9b5b1e1fb936df5e4
Worldspawn <worldspawn.web@gmail.com>
2024-02-27 20:25:29 +0300

    feat: MainPage (testing state)

---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import { ActiveButtonIcon } from '../../common/buttons/icons/ActiveButtonIcon';
+import { ButtonIcon } from '../../common/buttons/icons/ButtonIcon';
+
+export const MainPage = () => {
+  // Testing Stage
+  // TODO: Move to Storybook instead
+  return (
+    <>
+      <ActiveButtonIcon />
+      <ButtonIcon />
+    </>
+  );
+};
